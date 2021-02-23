a5ddac4274d5f6518ec676848de953b525a7b923
Iman <hussa703@uni.coventry.ac.uk>
2021-02-23 17:24:43 +0000

    Initial commit

---
+++ src/pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <f7-page name="about">
+    <f7-navbar title="About" back-link="Back"></f7-navbar>
+    <f7-block-title>About OneRemote</f7-block-title>
+    <f7-block strong>
+      <p>OneRemote recommends you with a bespoke remote tool for you to be able to WFH(work from home) and talk to your loved ones.</p>
+    </f7-block>
+    <f7-block>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni molestiae laudantium dignissimos est nobis delectus nemo ea alias voluptatum architecto, amet similique, saepe iste consectetur in repellat ut minus quibusdam!</p>
+    </f7-block>
+  </f7-page>
+</template>
---
+++ src/pages/home.vue
@@ -0,0 +1,59 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar large :sliding="false">
+
+
+      <f7-nav-title sliding>OneRemote</f7-nav-title>
+      <f7-nav-right>
+        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
+      </f7-nav-right>
+      <f7-nav-title-large>OneRemote</f7-nav-title-large>
+    </f7-navbar>
+    <!-- Toolbar-->
+    <f7-toolbar bottom>
+      <p>Copyright OneRemote ©</p>
+    </f7-toolbar>
+    <!-- Page content-->
+    <f7-block strong>
+      <p>This is an example of split view application layout, commonly used on tablets. The main approach of such kind of layout is that you can see different views at the same time.</p>
+
+      <p>Each view may have different layout, different navbar type (dynamic, fixed or static) or without navbar.</p>
+
+      <p>The fun thing is that you can easily control one view from another without any line of JavaScript just using "data-view" attribute on links.</p>
+    </f7-block>
+    <f7-block-title>Contact Developer</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/about/" title="About"></f7-list-item>
+      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    </f7-list>
+
+    <f7-block-title>Sign in/Sign up</f7-block-title>
+    <f7-block strong>
+      <f7-row>
+        <f7-col width="50">
+          <f7-button fill raised popup-open="#my-popup">Sign up</f7-button>
+        </f7-col>
+        <f7-col width="50">
+          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+
+
+    <f7-list>
+      <f7-list-item
+        title="Dynamic (Component) Route"
+        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
+      ></f7-list-item>
+      <f7-list-item
+        title="Default Route (404)"
+        link="/load-something-that-doesnt-exist/"
+      ></f7-list-item>
+      <f7-list-item
+        title="Request Data & Load"
+        link="/request-and-load/user/123456/"
+      ></f7-list-item>
+    </f7-list>
+  </f7-page>
+</template>
