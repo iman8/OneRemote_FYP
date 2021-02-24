84df924d1a7c7536be3dc673de8ae574971c8154
Iman <hussa703@uni.coventry.ac.uk>
2021-02-24 15:47:26 +0000

    Home Page Section under menu created
    The  first section under header is created for the home page.

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -15,6 +15,24 @@
       <p>Copyright OneRemote ©</p>
     </f7-toolbar>
     <!-- Page content-->
+        <!-- includes card-expandable -->
+<div class="card card-expandable">
+  <!-- card content -->
+  <div class="card-content">
+     <div class="bg-color-white" style="height: 300px">
+            <div class="card-header text-color-black display-block">OneRemote<br />
+              <small style="opacity: 0.7">Recommending you all tools suitable for you</small>
+            </div>
+            <a href="#" class="link card-close card-opened-fade-in color-black"
+              style="position: absolute; right: 15px; top: 15px">
+              <i class="icon f7-icons">xmark_circle_fill</i>
+            </a>
+          </div>
+    <div class="card-header">OneRemote</div>
+       <p>Missing your loved ones?Need a way to contact them? Are you in need of a tool to work from home? You are in the correct place!</p>
+           <button class="col button button-outline button-round">Find your tool!</button>
+  </div>
+</div>
     <f7-block strong>
       <p>This is an example of split view application layout, commonly used on tablets. The main approach of such kind of layout is that you can see different views at the same time.</p>
 
@@ -39,13 +57,6 @@
       </f7-row>
     </f7-block>
     
-    <!-- includes card-expandable -->
-<div class="card card-expandable">
-  <!-- card content -->
-  <div class="card-content">
-    <!-- put all content here -->
-  </div>
-</div>
 
 
     <f7-list>
