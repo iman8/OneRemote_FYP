e09305afb003eb156a291bcbfc1e64fbf0f7afdb
Iman <hussa703@uni.coventry.ac.uk>
2021-02-24 14:48:19 +0000

    Update app.vue
    Error fixed on the code

--- src/components/app.vue
+++ src/components/app.vue
@@ -9,7 +9,7 @@
         <f7-list>
           <f7-list-item link="/about/" view=".view-main" panel-close title="About"></f7-list-item>
           <f7-list-item link="/form/" view=".view-main" panel-close title="Form"></f7-list-item>
-          <f7-list-item link="#" view=".view-main" back panel-close title="Back in history"></f7-list-item>
+          <f7-list-item link="#" view=".view-main" back panel-close title="Remote Tools"></f7-list-item>
         </f7-list>
       </f7-page>
     </f7-view>
@@ -22,15 +22,29 @@
     <!-- Popup -->
     <f7-popup id="my-popup">
       <f7-view>
-        <f7-page>
-          <f7-navbar title="Popup">
-            <f7-nav-right>
+        <f7-page sign-up>
+          <f7-navbar title="Sign up">
+           <f7-nav-right>
               <f7-link popup-close>Close</f7-link>
-            </f7-nav-right>
-          </f7-navbar>
+             </f7-nav-right>
+           </f7-navbar>
           <f7-block>
-            <p>Popup content goes here.</p>
+              <f7-list form>
+            <f7-list-input
+              type="text"
+              name="username"
+              placeholder="Your username"
+              v-model:value="username"
+            ></f7-list-input>
+            <f7-list-input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              v-model:value="password"
+            ></f7-list-input>
+          </f7-list>
           </f7-block>
+          <f7-list-button title="Sign In" @click=""></f7-list-button>
         </f7-page>
       </f7-view>
     </f7-popup>
@@ -38,7 +52,10 @@
     <f7-login-screen id="my-login-screen">
       <f7-view>
         <f7-page login-screen>
+          <f7-nav-right>
           <f7-login-screen-title>Login</f7-login-screen-title>
+            <f7-link my-login-screen-close>Close</f7-link>
+            </f7-nav-right>
           <f7-list form>
             <f7-list-input
               type="text"
@@ -56,7 +73,6 @@
           <f7-list>
             <f7-list-button title="Sign In" @click="alertLoginData"></f7-list-button>
             <f7-block-footer>
-              Some text about login information.<br>Click "Sign In" to close Login Screen
             </f7-block-footer>
           </f7-list>
         </f7-page>
